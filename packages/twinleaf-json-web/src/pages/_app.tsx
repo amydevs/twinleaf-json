3a40ab6328a4b53a68177d237f50200d6f4c02f9
amy <ctrlalternativedelete@gmail.com>
2025-01-18 13:42:50 +1100

    fix: favicon

--- packages/twinleaf-json-web/src/pages/_app.tsx
+++ packages/twinleaf-json-web/src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType, type AppProps } from "next/app";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <link rel="shortcut icon" href={`${router.basePath}/favicon.ico`} />
+      </Head>
+      <div className={GeistSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 };
 
