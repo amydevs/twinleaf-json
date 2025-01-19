2eaf48c5888d0254f955281d377edfa25bcbf278
amy <ctrlalternativedelete@gmail.com>
2025-01-19 15:31:48 +1100

    fix: eslint disable rules

--- packages/twinleaf-json-web/src/mdx-components.tsx
+++ packages/twinleaf-json-web/src/mdx-components.tsx
@@ -6,8 +6,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     img: ({ src, ...imgProps }) => {
       // eslint-disable-next-line react-hooks/rules-of-hooks
       const router = useRouter();
-      // eslint-disable-next-line @next/next/no-img-element
-      // eslint-disable-next-line jsx-a11y/alt-text
+      // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
       return <img src={`${router.basePath}${src}`} {...imgProps} />;
     },
     ...components,
