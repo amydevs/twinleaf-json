02ae3a05c8f2a0245edf9128064f79b88af8580f
amy <ctrlalternativedelete@gmail.com>
2025-01-21 16:56:10 +1100

    fix: tags for source names

--- packages/twinleaf-json/index.ts
+++ packages/twinleaf-json/index.ts
@@ -34,18 +34,6 @@ async function main(args: string[]) {
       extract: () => Promise<Record<string, string>>,
     }>
   }> = {
-    "TCGDex (English + Japanese Cards, Canary)": {
-      variants: {
-        "Hi-Res Images": {
-          filePath: "tcgdex/large.json",
-          extract: () => tcgdexUtils.extract({ imageSize: "large" })
-        },
-        "Low-Res Images": {
-          filePath: "tcgdex/small.json",
-          extract: () => tcgdexUtils.extract({ imageSize: "small" })
-        }
-      }
-    },
     "Pokemon TCG API (English Cards Only, Stable)": {
       variants: {
         "Hi-Res Images": {
@@ -55,6 +43,18 @@ async function main(args: string[]) {
         "Low-Res Images": {
           filePath: "ptcgapi/small.json",
           extract: () => ptcgApiUtils.extract({ imageSize: "small" })
+        }
+      }
+    },
+    "TCGDex (English + Japanese Cards, Canary)": {
+      variants: {
+        "Hi-Res Images": {
+          filePath: "tcgdex/large.json",
+          extract: () => tcgdexUtils.extract({ imageSize: "large" })
+        },
+        "Low-Res Images": {
+          filePath: "tcgdex/small.json",
+          extract: () => tcgdexUtils.extract({ imageSize: "small" })
         }
       }
     },
